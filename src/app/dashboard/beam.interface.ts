7ae7866fea4db759a286b8ce2482bfca728fa2a5
Amal654 <19amal99@gmail.com>
2024-02-29 19:31:55 +0530

    [MNT] added interface

---
+++ src/app/dashboard/beam.interface.ts
@@ -0,0 +1,40 @@
+type SupportType = 'fixed' | 'roller' | 'pinned' | 'hing';
+type LoadType = 'fixed' | 'distributed' | 'triangular' | 'moment';
+
+interface BaseLoad {
+  type: LoadType;
+  position: number;
+}
+
+interface FixedLoad extends BaseLoad {
+  value: number;
+  angle: number;
+}
+
+interface DistributedLoad extends BaseLoad {
+  value: number;
+  start: number;
+  end: number;
+}
+
+interface TriangularLoad extends BaseLoad {
+  start: number;
+  end: number;
+  startValue: number;
+  endValue: number;
+}
+
+interface MomentLoad extends BaseLoad {
+  value: number;
+}
+
+type Load = FixedLoad | DistributedLoad | TriangularLoad | MomentLoad;
+
+export interface Beam {
+  length: number;
+  support: {
+    type: SupportType;
+    position: number;
+  }[];
+  load: Load[];
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
-
+import { Beam } from './beam.interface'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  screen:string=''
-  mainScreen:string=''
-  loggedInUsername : string = '';
-  constructor(private sharedService: SharedService){
+  screen: string = ''
+  mainScreen: string = ''
+  loggedInUsername: string = '';
+  myBeam: Beam = {
+    length: 10,
+    support: [
+      { type: 'fixed', position: 0 },
+      // { type: 'roller', position: 5 },
+      // { type: 'pinned', position: 10 },
+    ],
+    load: [
+      // { type: 'fixed', value: 100, position: 2, angle: 90 },
+      { type: 'distributed', value: 50, start: 3, end: 7, position: 4 },
+      // { type: 'triangular', start: 4, end: 8, startValue: 20, endValue: 40, position: 6 },
+      // { type: 'moment', value: 200, position: 9 },
+    ],
+  };
+  constructor(private sharedService: SharedService) {
 
   }
   ngOnInit() {
